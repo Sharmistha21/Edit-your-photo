3ecb549ac84c2897f05d50fd8b8b4178a48043a7
Sharmistha21 <sharmistha.chak21@gmail.com>
2025-09-05 19:49:39 +0530

    commits

---
+++ components/header.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import React from 'react'
+ 
+ const header = () => {
+   return (<header className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 text-nowrap">
+    <div className="backdrop-blur-md bg-white/10 border-white/20 rounded-full px-8 py-3
+    flex items-center justify-between gap-8">
+      <Link href="/" className="mr-10 md:mr-20" ><Image 
+      src="/logo-text.png"
+       alt="Pixxel logo"
+       className="min-w-24 object-cover"
+       width={96}
+       height={24} /></Link></div></header>);
+ };
+
+ export default header;
+ 
